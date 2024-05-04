e1525efaa52309c1de1f6cb1a86bc47de39f942e
Caleb Rowland <cjrowland4@gmail.com>
2024-05-04 11:27:00 -0500

    Add problem 2.52

--- Chapter 2 - Building Abstractions with Data/Exercises_2_2.rkt
+++ Chapter 2 - Building Abstractions with Data/Exercises_2_2.rkt
@@ -72,6 +72,24 @@
 (define (make-frame origin-vector edge-vector1 edge-vector2)
   (list frame origin-vector edge-vector1 edge-vector2))
 
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Exercise 2.17
@@ -870,7 +888,22 @@ NOTE 2: After digging into the "product of consecutive factorials idea" a litle 
 
 
 
-  
+; Exercise 2.52
+; Adding a smile to wave would simply require adding more line segments (probably 5 or so, for a decent smile) to the 19 segments described in Exercise 2.49d
 
+; Changed corner-split by adding a 90 degree rotation to the up-split part of the image
+(define (corner-split-change painter n)
+  (if (= n 0)
+      painter
+      (let ((up (rotate90 (up-split painter (- n 1))))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split-change painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
 
-
+; Changed the horizontal rotation to a vertical one, and the 180 degree rotation to a 270 degree rotation.
+(define (square-limit-abstraction painter n)
+  (let ((combine4 (square-of-four flip-vert identity rotate270 flip-vert)))
+    (combine4 (corner-split-change painter n))))
