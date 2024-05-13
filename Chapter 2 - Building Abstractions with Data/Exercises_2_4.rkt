ff7bd53cf2dbcbfb6259b00019dc121e62308904
Caleb Rowland <cjrowland4@gmail.com>
2024-05-13 12:55:04 -0500

    Add exercise 2.74

--- Chapter 2 - Building Abstractions with Data/Exercises_2_4.rkt
+++ Chapter 2 - Building Abstractions with Data/Exercises_2_4.rkt
@@ -68,6 +68,16 @@
 (define (get op type)  ; Placeholder for get operation, discussed more in chapter 3
   ("bar"))
 
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG")))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Exercise 2.73
@@ -127,6 +137,55 @@ a) In the data-directed rewrite, the operator is being stripped from the express
 
 ; d)  If the get procedure's argument order changed to ((get (operator exp) 'deriv) (operands exp) var), we would
 ;        have to change our put procedure calls from (put 'deriv '+ deriv+) to (put '+ 'deriv deriv+)
+
+
+
+; Exercise 2.74
+; Our packages will be dividied up by division. Each division's files should be structured as a list of files,
+;   with the first element being the employee name.
+(define (install-division<num>-package)
+  (define (get-record name)  ; This procedure will need an employee's name as an argument, for the generic, the division number and name will need to be supplied.
+    "Search Division <num>'s records for the record with name <name>. Returns #f if the record isn't found.")
+  (define (get-salary record)  ; This procedure will need an employee record as an argument, that should be a list with the employee name as the first element.
+    "Search the given Division <num> record for the salary information")
+
+  (put 'get-record '(<num>) get-record)
+  (put 'get-salary '(<num>) get-salary))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error
+           "No method for these types: APPLY-GENERIC"
+           (list op type-tags))))))
+
+; a)
+(define (get-record-generic division-number name)
+  (apply-generic 'get-record (list division-number name)))
+
+; b)
+(define (get-salary-generic division-number record)
+  (apply-generic 'get-salary (list division-number record)))
+
+; c)
+(define (find-employee-record name)
+  (define (get-record-iter current-division-number)
+    (let ((record (get-record-generic current-division-number name)))
+      (if (eq? record false)
+          (get-record-iter current-division-number)
+          record)))
+  (get-record-iter 1))
+
+; d) An item will have to be added to the beginning of the list that is the personnel file, indicating a number.
+;     Then each employee file will need to have the name of the employee added as the first element in the record.
+;     An installation package will also need to be created for the new division
+          
+  
+    
+
+
     
     
     
