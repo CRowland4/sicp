dcfa8519031140497b93ea8a2c5fc51789f0d758
Caleb Rowland <cjrowland4@gmail.com>
2024-05-15 14:53:55 -0500

    Add problem 3.2

--- Chapter 3 - Modularity, Objects, and State/Exercises_3_1.rkt
+++ Chapter 3 - Modularity, Objects, and State/Exercises_3_1.rkt
@@ -4,3 +4,18 @@
     (lambda (amount)
       (begin (set! value (+ value amount))
              value)))
+
+
+
+; Exercise 3.2
+(define (make-monitored f)
+  (let ((counter 0))
+    (define (mf x)
+      (cond ((eq? x 'how-many-calls?) counter)
+            ((eq? x 'reset-count)
+             (begin (set! counter 0)
+                    "Counter reset to 0"))
+            (else
+             (begin (set! counter (+ counter 1))
+                    (f x)))))
+    mf))
