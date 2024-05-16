20c576ca795cabd053ba86407aa31285f6ac73d5
Caleb Rowland <cjrowland4@gmail.com>
2024-05-15 19:10:44 -0500

    Add problem 3.5

---
+++ Chapter 2 - Building Abstractions with Data/code_follow_along_chapter_3.rkt
@@ -0,0 +1,111 @@
+#lang scheme
+; A variable with state
+(define balance 100)
+(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount)) ; the (set! balance (- balance amount)) expression decrements the balance
+             balance)
+    "Insufficient funds"))
+
+; Redefining withdraw so that balance is an internal variable
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+; A withdraw procedure with "withdrawal processors"
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+(define W2 (make-withdraw 100))
+(W1 50)
+(W2 70)
+(W2 40)
+(W1 40)
+
+
+
+; A "bank-account object" with an initial balance specified as input
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+; Use of make-account
+(define acc (make-account 100))
+((acc 'withdraw) 50)
+((acc 'withdraw) 60)
+((acc 'deposit) 40)
+((acc 'withdraw) 60)
+
+; Completely new object with a separate balance
+(define acc2 (make-account 100))
+
+
+
+; Random integer procedure
+(define random-init 4)  ; To avoid errors
+(define (rand-update x) (+ x 1))  ; Just here to avoid errors, this isn't what this procedure would look like
+(define rand (let ((x random-init))
+               (lambda ()
+                 (set! x (rand-update x))
+                 x)))
+
+
+
+; Monte Carlo procedure fir estimating pi
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1)
+                 trials-passed))))
+    (iter trials 0))
+
+; Same procedure, but using rand-update directly instead of rand
+(define (estimate-pi-rand-update trials)
+  (sqrt (/ 6 (random-gcd-test trials random-init))))
+(define (random-gcd-test trials initial-x)
+  (define (iter trials-remaining trials-passed x)
+    (let ((x1 (rand-update x)))
+      (let ((x2 (rand-update x1)))
+        (cond ((= trials-remaining 0)
+               (/ trials-passed trials))
+              ((= (gcd x1 x2) 1)
+               (iter (- trials-remaining 1)
+                     (+ trials-passed 1)
+                     x2))
+              (else
+               (iter (- trials-remaining 1)
+                     trials-passed
+                     x2))))))
+  (iter trials 0 random-init))
+      
--- Chapter 3 - Modularity, Objects, and State/Exercises_3_1.rkt
+++ Chapter 3 - Modularity, Objects, and State/Exercises_3_1.rkt
@@ -69,3 +69,40 @@
           (else (error "Unknown request: MAKE-ACCOUNT"
                        m))))
   dispatch)
+
+
+
+; Exercise 3.5
+(define (square x) (* x x))
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1)
+                 trials-passed))))
+    (iter trials 0))
+
+ (define (random-in-range low high) 
+   (let ((range (- high low))) 
+     (+ low (* (random) range)))) 
+
+(define (estimate-integral P x1 x2 y1 y2 trials)
+  (define (in-area)
+    (P (random-in-range x1 x2)
+       (random-in-range y1 y2)))
+  (let ((rectangle-area (* (- x2 x1) (- y2 y1))))
+   (* (monte-carlo trials in-area) rectangle-area 1.0)))
+
+(define (unit-circle-predicate x y)
+  (<= (+ (square (- x 2)) (square (- y 2))) 1))
+
+(define (pi-estimate)
+  (estimate-integral unit-circle-predicate 0.0 4.0 0.0 4.0 5000000))
+
+
+  
+  
