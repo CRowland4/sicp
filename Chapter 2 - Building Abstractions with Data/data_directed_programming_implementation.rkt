b3cca40e07693d08175d5fbe86bcb194d2a63090
Caleb Rowland <cjrowland4@gmail.com>
2024-05-27 18:14:28 -0500

    Add exercise 2.87 to arithmetic system

--- Chapter 2 - Building Abstractions with Data/data_directed_programming_implementation.rkt
+++ Chapter 2 - Building Abstractions with Data/data_directed_programming_implementation.rkt
@@ -286,12 +286,89 @@ Every variation asked for in the exercises won't be present, but anytime sometin
        (lambda (r a) (tag (make-from-mag-ang r a))))
   "Complex package installed")
 
+;The polynomial package
+(define (install-polynomial-package)
+  ; internal procedures
+  ; representation of poly
+  (define (make-poly variable term-list) (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  (define (variable? x)
+    (symbol? x))
+  ; representation of terms and term lists
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: ADD-POLY"(list p1 p2))))
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1))
+                 (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1 (add-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2 (add-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term (order t1)
+                                (add (coeff t1) (coeff t2)))
+                     (add-terms (rest-terms L1)
+                                (rest-terms L2)))))))))
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1) (term-list p2)))
+        (error "Polys not in the same var: MUL-POLY" (list p1 p2))))
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term (+ (order t1) (order t2))
+                      (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+  (define (zero-poly? p)  ; Exercise 2.87
+    (define (iter terms)
+      (cond ((empty-termlist? terms) true)
+            ((not (= (coeff (first-term terms)) 0)) false)
+            (else (iter (rest-terms terms)))))
+    (iter (term-list p)))
+  ; interface to rest of system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put '=zero? '(polynomial) zero-poly?)
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
+  
 
-
-
-
-
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Package installations
 (install-rectangular-package)
@@ -299,6 +376,7 @@ Every variation asked for in the exercises won't be present, but anytime sometin
 (install-scheme-number-package)
 (install-rational-package)
 (install-complex-package)
+(install-polynomial-package)
 
 
 ; Rectangular and polar complex number generics
@@ -336,6 +414,8 @@ Every variation asked for in the exercises won't be present, but anytime sometin
   ((get 'make-from-real-imag 'complex) x y))
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
 
 
 ; Coercion procedures
@@ -432,6 +512,16 @@ Every variation asked for in the exercises won't be present, but anytime sometin
 (magnitude zr2)
 (angle zr1)
 (angle zr2)
+
+; Polynomial operations
+(define px (make-polynomial 'x (list (list 4 1) (list 3 2) (list 2 3) (list 1 4) (list 0 5))))
+(define px2 (make-polynomial 'x (list (list 7 12) (list 6 9) (list 5 43) (list 4 1) (list 3 2) (list 2 3) (list 1 4) (list 0 5))))
+(define py (make-polynomial 'y (list (list 4 1) (list 3 2) (list 2 3) (list 1 4) (list 0 5))))
+(define p-zero (make-polynomial 'z (list (list 2 0) (list 1 0) (list 0 0))))
+(add px px2)
+(mul px px2)
+(=zero? p-zero)
+(=zero? px)
 
 ; Coercion operations
 (scheme-number->complex 5)
