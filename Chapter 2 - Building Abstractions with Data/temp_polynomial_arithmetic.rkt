659463d768287d73da3eaff3e5cb5a3ca57ea37f
Caleb Rowland <cjrowland4@gmail.com>
2024-05-30 12:15:31 -0500

    Add progress on general arithmetic system. Got Add-poly working correctly with different termlist types

--- Chapter 2 - Building Abstractions with Data/temp_polynomial_arithmetic.rkt
+++ Chapter 2 - Building Abstractions with Data/temp_polynomial_arithmetic.rkt
@@ -134,73 +134,19 @@
 
 
 
-; The sparse package
-(define (install-sparse-package)
-  ; internal procedures
-  (define (add-terms L1 L2)
-    (cond ((empty-termlist? L1) L2)
-          ((empty-termlist? L2) L1)
-          (else
-           (let ((t1 (first-term L1))
-                 (t2 (first-term L2)))
-             (cond ((> (order t1) (order t2))
-                    (adjoin-term
-                     t1 (add-terms (rest-terms L1) L2)))
-                   ((< (order t1) (order t2))
-                    (adjoin-term
-                     t2 (add-terms L1 (rest-terms L2))))
-                   (else
-                    (adjoin-term
-                     (make-term (order t1)
-                                (add (coeff t1) (coeff t2)))
-                     (add-terms (rest-terms L1)
-                                (rest-terms L2)))))))))
-
-  ; interface to rest of the system
-
-
-
-  "Sparse package installed")
 
 
 
 
-; The dense package
-(define (install-dense-package)
-  ; internal procedures
-
-  ; interface to rest of the system
 
 
 
-  "Dense package installed")
 
 
 
-;The polynomial package
-(define (install-polynomial-package)
-  ; internal procedures
-  (define (variable? x)
-    (symbol? x))
-  (define (same-variable? v1 v2)
-    (and (variable? v1)
-         (variable? v2)
-         (eq? v1 v2)))
-  (define (add-poly p1 p2)
-    (if (same-variable? (variable p1) (variable p2))
-        (make-poly (variable p1)  ; TODO make-poly to a more specific make
-                   (add-terms (term-list p1) (term-list p2)))
-        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
-  (define (mul-poly p1 p2)
-    (if (same-variable? (variable p1) (variable p2))
-        (make-poly (variable p1)  ; TODO make-poly to a more specific make
-                   (mul-terms (term-list p1) (term-list p2)))
-        (error "Polys not in same var: MUL-POLY" (list p1 p2))))
-  ; interface to rest of the system
-  
 
-  
-    "Polynomial package installed")
+
+
 
 
 
@@ -234,30 +180,15 @@
     (car term-list))
   (define (rest-terms term-list)
     (cdr term-list))
-  (define (make-term order coeff)
-    (list order coeff))
+  
   (define (order term)
     (car term))
   (define (coeff term)
     (cadr term))
 
-  (define (mul-terms L1 L2)
-    (if (empty-termlist? L1)
-        (the-empty-termlist)
-        (add-terms (mul-term-by-all-terms (first-term L1) L2)
-                   (mul-terms (rest-terms L1) L2))))
-  (define (mul-term-by-all-terms t1 L)
-    (if (empty-termlist? L)
-        (the-empty-termlist)
-        (let ((t2 (first-term L)))
-          (adjoin-term
-           (make-term (+ (order t1) (order t2))
-                      (mul (coeff t1) (coeff t2)))
-           (mul-term-by-all-terms t1 (rest-terms L))))))
-  (define (adjoin-term term term-list)
-    (if (=zero? (coeff term))
-        term-list
-        (cons term term-list)))
+  
+  
+  
   (define (make-from-sparse-terms var sparse-terms)
     (cons var sparse-terms))
   (define (create-sparse-terms-from-dense-terms dense-terms)
@@ -325,5 +256,3 @@
       
   ; interface to rest of the system
 |#
-
-(define (variable p) (apply-generic 'variable p))
