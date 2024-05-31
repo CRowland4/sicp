0af9d6496112290e3e8d717bafd7684083b3fcde
Caleb Rowland <cjrowland4@gmail.com>
2024-05-31 12:24:53 -0500

    Add exercise 3.24

--- Chapter 3 - Modularity, Objects, and State/Exercises_3_3.rkt
+++ Chapter 3 - Modularity, Objects, and State/Exercises_3_3.rkt
@@ -272,3 +272,42 @@ To print the queue like we'd expect, we just need to see what the front pointer 
          (set-next! (prev (dq-rear-ptr dq)) '())
          (set-dq-rear-ptr! dq (prev (dq-rear-ptr dq)))
          (print-dq dq))))
+
+
+
+; Exercise 3.24
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record (cdr record) false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+        (set-cdr! local-table
+                  (cons (list key-1 (cons key-2 value))
+                        (cdr local-table)))))
+      'ok)
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records))
+             (car records))
+            (else (assoc key (cdr records)))))
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
